059bdb5f5b823a9c9cfdcd1101cc75678a2e5520
PatrikJMelander <patrikjmelander@gmail.com>
2021-05-27 16:24:53 +0200

    change of url

--- url.js
+++ url.js
@@ -1,8 +1,8 @@
 // for localhost
-//const startUrl = 'https://go-north.herokuapp.com'
+//const startUrl = 'https://localhost/8080:'
 
 // for heruko 
-const startUrl = 'https://alm-pedometer.herokuapp.com'
+const startUrl = 'https://go-north.herokuapp.com'
 
 let validateLogin = `${startUrl}/api/v1/users/validate/`
 let getStepsOfUser = `${startUrl}/api/v1/users/get/one?email=`
